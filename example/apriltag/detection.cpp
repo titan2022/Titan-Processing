ec85eaa8dd3bfcb2a1ba78469a951d9f2da3683f
nekiwo <nekiwo@users.noreply.github.com>
2023-07-18 11:32:25 -0500

    put load on localizer to deal with data

--- example/apriltag/detection.cpp
+++ example/apriltag/detection.cpp
@@ -4,24 +4,20 @@
 
 #include "../../include/apriltag/Apriltag.hpp"
 #include "../../include/apriltag/ApriltagDetector.hpp"
+#include "../../include/apriltag/Localizer.hpp"
 #include "../../include/helper/ConfigReader.hpp"
 #include "../../include/networking/Client.hpp"
 
-void handleApriltag(const Apriltag &tag, NetworkingClient &client)
-{
-    std::cout << std::to_string(tag.id) << ": " << tag.position.toString() << std::endl;
-    client.send_data("test", false, tag.position);
-}
-
 int main(int argc, char const *argv[])
 {
-    NetworkingClient client("127.0.0.1", 5800);
-    ConfigReader config("example/processing.cfg");
-;
-    ApriltagDetector detector(0, true, config, client);
+    ConfigReader config("./example");
+    NetworkingClient client(config.ip, config.port);
+    
+    Localizer localizer(config, client);
+    ApriltagDetector detector(0, true, config, localizer);
 
     detector.startStream();
-    detector.detect(handleApriltag);
+    detector.detect();
     
     return 0;
 }
