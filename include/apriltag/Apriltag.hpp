58fb5f1dd949ed381582dffa0e827af78ada9d30
nekiwo <nekiwo@users.noreply.github.com>
2023-07-18 11:33:54 -0500

    remove pose

--- include/apriltag/Apriltag.hpp
+++ include/apriltag/Apriltag.hpp
@@ -3,7 +3,6 @@
 
 #include <string>
 
-#include "../helper/Pose.hpp"
 #include "../helper/Vector3D.hpp"
 
 class Apriltag
@@ -11,7 +10,6 @@ class Apriltag
     public:
         Vector3D &position;
         Vector3D &rotation;
-        Pose3D *pose;
         int id;
         Apriltag(int id, Vector3D &position, Vector3D &rotation);
 };
--- src/apriltag/Apriltag.cpp
+++ src/apriltag/Apriltag.cpp
@@ -1,8 +1,7 @@
 #include "apriltag/Apriltag.hpp"
-#include "helper/Pose.hpp"
 #include "helper/Vector3D.hpp"
 
 Apriltag::Apriltag(int id, Vector3D &position, Vector3D &rotation) : id(id), position(position), rotation(rotation)
 {
-    this->pose = new Pose3D(&position, &rotation);
+    
 }
