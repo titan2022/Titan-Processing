1caefd0f3cf79b4b15b27b04a8e78bc2f5794472
j4d35t0n3 <94765041+j4d35t0n3@users.noreply.github.com>
2024-03-01 15:07:36 -0600

    Updated networking examples

--- example/networking/with_reply.cpp
+++ example/networking/with_reply.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <string>
 
-#include "../../include/networking/Client.h"
 #include "../../include/helper/Vector3D.hpp"
+#include "../../include/networking/Client.h"
 
-int main(int argc, char const *argv[])
-{
-    std::string ip = "127.0.0.1";
-    int port = 5800;
-    Vector3D v(0, 1, 2);
+int main(int argc, char const *argv[]) {
+  std::string ip = "127.0.0.1";
+  int port = 5800;
+  Vector3D v(0, 1, 2);
 
-    NetworkingClient client(ip, port);
-    client.send_vector("test", v, false);
-    
-    std::cout << "Vector " << v.toString() << " sent to " << ip << ":" << port << std::endl;
+  NetworkingClient client(ip, port);
+  client.send_vector("test", v, false);
 
-    return 0;
+  std::cout << "Vector " << v.toString() << " sent to " << ip << ":" << port
+            << std::endl;
+
+  return 0;
 }
--- example/networking/with_reply_c.c
+++ example/networking/with_reply_c.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
 #include "../../include/networking/Client.h"
 #include <stdbool.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    char* ip = "127.0.0.1";
-    int port = 5800;
-    TRBVector3D v = (TRBVector3D){0, 1, 2};
+int main(int argc, char const *argv[]) {
+  char *ip = (char *)"127.0.0.1";
+  int port = 5800;
+  TRBVector3D v = (TRBVector3D){0, 1, 2};
 
-    TRBNetworkingClientRef client = TRBNetworkingClientCreate(ip, port);
-    TRBVector3D reply = TRBNetworkingClientSendVector(client, "test", v, true);
-    
-    printf("Vector <%f, %f, %f> sent to %s:%d, reply is <%f, %f, %f>\n", v.x, v.y, v.z, ip, port, reply.x, reply.y, reply.z);
+  TRBNetworkingClientRef client = TRBNetworkingClientCreate(ip, port);
+  TRBVector3D reply =
+      TRBNetworkingClientSendVector(client, (char *)"test", v, true);
 
-    return 0;
+  printf("Vector <%f, %f, %f> sent to %s:%d, reply is <%f, %f, %f>\n", v.x, v.y,
+         v.z, ip, port, reply.x, reply.y, reply.z);
+
+  return 0;
 }
