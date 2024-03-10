3f5845041b6ff3443e7b22f572cfc4eb36d4016d
nekiwo <58634064+nekiwo@users.noreply.github.com>
2024-03-10 15:35:05 -0500

    wip asjdalhbdasd

--- example/helper/undistort.cpp
+++ example/helper/undistort.cpp
@@ -8,27 +8,30 @@
 
 #include "../../include/helper/ConfigReader.hpp"
 
+constexpr int CAM_ID = 2;
+constexpr int CAM_CONFIG_INDEX = 1;
+
 int main(int argc, char const *argv[])
 {
     ConfigReader config("../example");
 
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(CAM_ID);
     if (!cap.isOpened())
     {
         std::cerr << "Couldn't open video capture device" << std::endl;
         return 0;
     }
 
-    cap.set(cv::CAP_PROP_FPS, config.cameras[0].fps);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, config.cameras[0].width);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, config.cameras[0].height);
+    // cap.set(cv::CAP_PROP_FPS, config.cameras[CAM_CONFIG_INDEX].fps);
+    // cap.set(cv::CAP_PROP_FRAME_WIDTH, config.cameras[CAM_CONFIG_INDEX].width);
+    // cap.set(cv::CAP_PROP_FRAME_HEIGHT, config.cameras[CAM_CONFIG_INDEX].height);
 
     while (true)
     {
         cv::Mat frame, out;
         cap >> frame;
 
-        cv::undistort(frame, out, config.cameras[0].cameraMat, config.cameras[0].distCoeffs);
+        cv::undistort(frame, out, config.cameras[CAM_CONFIG_INDEX].cameraMat, config.cameras[CAM_CONFIG_INDEX].distCoeffs);
 
         cv::imshow("Apriltag Debug Window", out);
         if (cv::waitKey(1) == 27) { // ESC key
