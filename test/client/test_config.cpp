280cc592c03e5cdedf3ef71955f86cf4d7ea5476
nekiwo <58634064+nekiwo@users.noreply.github.com>
2025-01-12 01:59:55 -0600

    fixed some parsing issues with config

--- test/client/test_config.cpp
+++ test/client/test_config.cpp
@@ -9,11 +9,14 @@ using namespace titan;
 
 TEST(ConfigTest, IO)
 {
-	fs::path configFolderPath = fs::current_path().parent_path() / "example";
+	fs::path configFolderPath = fs::current_path().parent_path() / "config";
+	fs::path configPath = configFolderPath / "config.json";
+	fs::path tagsPath = configFolderPath / "apriltags2025.json";
 	ConfigReader config;
-	int error = config.readFromFile(configFolderPath.string());
+	int error = config.readFromFile(configPath.string(), tagsPath.string());
 
-	ASSERT_NE(error, 2) << "IO Error 2: Config file not found inside " << configFolderPath.string();
+	ASSERT_NE(error, 2) << "IO Error 2: Config file(s) not found." << configFolderPath.string();
+	ASSERT_NE(error, 5) << "IO Error 5: Config file(s) could not be read. " << configFolderPath.string();
 	ASSERT_EQ(error, 0) << "IO Error " << std::to_string(error) << ": Unknown error with loading config file.";
 
 	ASSERT_EQ(config.ip, "127.0.0.1") << "Config Error: incorrect IP parsing.";
