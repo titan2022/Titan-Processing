8fca2fe1e2ba94d39ee0ffaabe801df0359d6a79
ethanc8 <echaroenpitaks@gmail.com>
2025-01-15 00:04:46 -0600

    Fix push scripts

--- scripts/host-orangepi/push.sh
+++ scripts/host-orangepi/push.sh
@@ -3,4 +3,6 @@
 source "$( dirname -- "${BASH_SOURCE[0]}" )/credentials.sh"
 
 sshpass -p "$password" ssh "$username@$remote_hostname" "cd ~/Projects/Titan-Processing && git checkout main --force"
-git push "$git_remote" HEAD:__titan_deployment_staging --force
+sshpass -p "$password" git push "$git_remote" HEAD:__titan_deployment_staging --force
+sshpass -p "$password" ssh "$username@$remote_hostname" "cd ~/Projects/Titan-Processing && git checkout __titan_deployment_staging"
+
