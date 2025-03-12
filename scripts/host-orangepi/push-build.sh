22ec8896377ce3d97758b588914e4baeb017d09a
ethanc8 <echaroenpitaks@gmail.com>
2025-03-12 10:17:57 -0500

    Fix push-build.sh

--- scripts/host-orangepi/push-build.sh
+++ scripts/host-orangepi/push-build.sh
@@ -2,7 +2,7 @@
 
 source "$( dirname -- "${BASH_SOURCE[0]}" )/credentials.sh"
 
-sshpass -p "$password" "$username@$remote_hostname" "cd ~/Projects/Titan-Processing && git checkout main --force"
+sshpass -p "$password" ssh "$username@$remote_hostname" "cd ~/Projects/Titan-Processing && git checkout main --force"
 sshpass -p "$password" git push "$git_remote" HEAD:__titan_deployment_staging --force
 sshpass -p "$password" ssh "$username@$remote_hostname" "cd ~/Projects/Titan-Processing && git checkout __titan_deployment_staging; ./scripts/orangepi/build.sh"
 
